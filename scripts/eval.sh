1567254e57d88903005885a4f211028639cb27ec
Escanord <anhduy6102@gmail.com>
2024-03-20 01:42:48 -0400

    update
    Signed-off-by: Escanord <anhduy6102@gmail.com>

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -37,14 +37,14 @@ for model in gcn_mlp; do ###gcn sage mlp gcn_mlp sage_mlp gat gat_mlp
         --stop_edit_only False \
         --stop_full_edit True \
         --half_half True \
-        --half_half_ratio_mixup 0.5 \
+        --half_half_ratio_mixup 0.25 \
         --iters_before_stop 0 \
         --full_edit 0 \
         --mixup_k_nearest_neighbors True \
         --incremental_batching True \
         --sliding_batching 0 \
         --pure_egnn 0 \
-        --wrong_ratio_mixup 0.5 \
+        --wrong_ratio_mixup 0.0 \
         --criterion ${criterion} 2>&1 | tee ${output_dir}/${dataset}/${manner}/${model}_${criterion}_eval.log
 done
 done
